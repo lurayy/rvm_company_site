d5950b275cca467f04c979b01e85d1a1d28369b2
Kabir S. Tamari <kabirtamari@gmail.com>
2023-02-13 02:27:45 +0545

    added out

---
+++ out/_next/static/wSN4EcamfpepMcHhDNjy_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/462-4fc4c3968546b730.js","static/chunks/pages/index-3ce3d22995ff4d73.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
